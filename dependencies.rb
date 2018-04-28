6cba35e1749dfad463977796b5918d1947b3fbff
Ondřej Moravčík <moravcik.ondrej@gmail.com>
2018-04-28 22:03:16 +0200

    ryspec

--- dependencies.rb
+++ dependencies.rb
@@ -1,14 +1,22 @@
-# Dependencies which are loaded in main application or in dummy application
-# All gems must be in "rys" group
+git_source(:easy_git){ |name| "git@git.easy.cz:platform-2.0/#{name}.git" }
 
 group :rys do
   group :default do
-    gem 'rys', github: 'easysoftware/rys', branch: 'master'
+    gem 'rys', easy_git: 'rys', branch: 'master'
   end
 
-  group :development do
+  group :test, :development do
+    gem 'rspec-rails', require: false
   end
 
   group :test do
+    gem 'faker'
+    gem 'launchy'
+    gem 'capybara'
+    gem 'rspec-rails'
+    gem 'poltergeist'
+    gem 'database_cleaner'
+    gem 'factory_bot_rails'
+    # gem 'selenium-webdriver'
   end
 end
--- spec/init_factory_bot.rb
+++ spec/init_factory_bot.rb
@@ -1,3 +1,7 @@
+# To prevent loading factories from old core
+FactoryBot.definition_file_paths = []
+FactoryBot.reset_configuration
+
 Rys::PluginsManagement.all do |plugin|
   FactoryBot.definition_file_paths << plugin.root.join('spec/factories')
 end
