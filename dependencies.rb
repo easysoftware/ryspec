235c710df2114c005f1a49b0fd934e914b72dfd5
Lukáš Pokorný <lukas.pokorny@easysoftware.cz>
2018-05-02 09:40:07 +0200

    reformat dependencies.rb

--- dependencies.rb
+++ dependencies.rb
@@ -1,22 +1,20 @@
-git_source(:easy_git){ |name| "git@git.easy.cz:platform-2.0/#{name}.git" }
 
 group :rys do
   group :default do
-    gem 'rys', easy_git: 'rys', branch: 'master'
+    gem 'rys', github: 'easysoftware/rys', branch: 'master'
   end
 
   group :test, :development do
-    gem 'rspec-rails', require: false
+    gem 'rspec-rails', '~> 3.7.2', require: false
   end
 
   group :test do
-    gem 'faker'
-    gem 'launchy'
-    gem 'capybara'
-    gem 'rspec-rails'
-    gem 'poltergeist'
-    gem 'database_cleaner'
-    gem 'factory_bot_rails'
-    # gem 'selenium-webdriver'
+    gem 'capybara', '~> 2.18.0'
+    gem 'database_cleaner', '~> 1.6.2'
+    gem 'factory_bot_rails', '~> 4.8.2'
+    gem 'faker', '~> 1.8.7'
+    gem 'launchy', '~> 2.4.3'
+    gem 'poltergeist', '~> 1.17.0'
+    gem 'webmock', '~> 3.4.1'
   end
 end
