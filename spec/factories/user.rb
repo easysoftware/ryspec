be7f02df105a1db1e9ac85939ceed866f8f02eda
Lukáš Pokorný <lukas.pokorny@easysoftware.cz>
2019-01-08 15:58:08 +0100

    add alias to user factory
    Signed-off-by: Lukáš Pokorný <lukas.pokorny@easysoftware.cz>

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
 
-  factory :user do
+  factory :user, aliases: [:author] do
     sequence(:firstname) { |n| "Firstname#{n}" }
     sequence(:lastname) { |n| "Lastname#{n}" }
     sequence(:mail) { |n| "user#{n}@example.net" }
