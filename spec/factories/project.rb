e792a48c625ad3ced90c6742105aa2073793f237
Lukáš Pokorný <lukas.pokorny@easysoftware.cz>
2019-03-19 11:33:58 +0100

    lint only own factories

--- spec/factories/project.rb
+++ spec/factories/project.rb
@@ -14,6 +14,7 @@ FactoryBot.define do
       trackers = Tracker.all.to_a if trackers.empty?
       trackers = [FactoryBot.create(:tracker)] if trackers.empty?
       project.trackers = trackers
+      project.project_time_entry_activities = FactoryBot.create_list(:time_entry_activity, 1) if project.project_time_entry_activities.empty?
 
       FactoryBot.create_list :issue, evaluator.number_of_issues, project: project
     end
--- spec/factory_spec.rb
+++ spec/factory_spec.rb
@@ -1,3 +1,20 @@
 RSpec.describe "Lint builtin factories" do
-  it { FactoryBot.lint }
+
+  def factories(names)
+    names.collect { |name| FactoryBot.factory_by_name(name) }
+  end
+
+  it { FactoryBot.lint factories(%i[custom_field easy_custom_field_group user]) }
+
+  context "Issue" do
+    it { FactoryBot.lint factories(%i[issue issue_custom_field journal tracker issue_status issue_priority]) }
+  end
+
+  context "Project" do
+    it { FactoryBot.lint factories(%i[project role]) }
+  end
+
+  context "TimeEntry" do
+    it { FactoryBot.lint factories(%i[time_entry time_entry_activity]) }
+  end
 end
