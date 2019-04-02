a01df32c9c2488e4d6085f73b3bc838ceb37d9e5
Joas1988 <Joas1988@gmail.com>
2019-04-02 11:10:23 +0200

    Update factory_spec.rb
    Tests

--- spec/factory_spec.rb
+++ spec/factory_spec.rb
@@ -9,9 +9,13 @@ RSpec.describe "Lint builtin factories" do
   context "Issue" do
     it { FactoryBot.lint factories(%i[issue issue_custom_field journal tracker issue_status issue_priority]) }
   end
+  
+   context "News" do
+    it { FactoryBot.lint factories(%i[news]) }
+  end
 
   context "Project" do
-    it { FactoryBot.lint factories(%i[project role]) }
+    it { FactoryBot.lint factories(%i[project role member member_role]) }
   end
 
   context "TimeEntry" do
