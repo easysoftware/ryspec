e8424e147a66c915dadf4bd47f4b51c10c5131cd
Joas1988 <Joas1988@gmail.com>
2019-04-02 09:27:37 +0200

    Project members added

--- spec/factories/project.rb
+++ spec/factories/project.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
   factory :project do
     transient do
       number_of_issues { 0 }
+      number_of_members { 0 }
       trackers { [] }
     end
 
@@ -17,6 +18,7 @@ FactoryBot.define do
       project.project_time_entry_activities = FactoryBot.create_list(:time_entry_activity, 1) if project.project_time_entry_activities.empty?
 
       FactoryBot.create_list :issue, evaluator.number_of_issues, project: project
+      FactoryGirl.create_list :member, evaluator.number_of_members, :project => project, :roles => [FactoryGirl.create(:role)]
     end
 
   end
