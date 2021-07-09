cf18c586838c91790e90c4d19f4cd2f789a7ed20
Lukas Pokorny <luk4s.pokorny@gmail.com>
2021-07-09 10:06:32 +0200

    RYS add feature to existing plugin and for this needs their Factories (#23)
    * RYS add feature to existing plugin and for this needs their Factories
    
    * improve naming

--- lib/ryspec.rb
+++ lib/ryspec.rb
@@ -3,4 +3,20 @@ require 'rys'
 require 'ryspec/engine'
 
 module Ryspec
+
+  # @param [String, Symbol] plugin_name of Redmine plugin (Symbol is preferred)
+  # @param [String] file_name of factory in `test/factories` (by default its name of plugin)
+  # @param [String, Symbol] factory_name of FactoryBot.define (by default its equal to filename)
+  #
+  # @example Ryspec.require_factory :easy_contacts, file_name: "easy_contact"
+  def self.require_factory(plugin_name, file_name: nil, factory_name: nil)
+    factory_name ||= file_name || plugin_name
+    return if FactoryBot.factories.registered? factory_name
+
+    factory_file = File.join(Redmine::Plugin.find(name).directory, "/test/factories/#{file_name || plugin_name}")
+    raise ArgumentError, "File `#{factory_file}` doesnt exists!" unless File.exist?(factory_file)
+
+    require factory_file
+  end
+
 end
